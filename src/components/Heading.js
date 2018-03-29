b7f39e91d8015e29fc8b76929964aa5910723b71
Josh Perez <josh@goatslacker.com>
2018-03-29 11:42:28 -0700

    Adds levels to Heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -6,9 +6,9 @@ import { Text, StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   heading: {
     fontFamily: 'Montserrat-SemiBold',
-    fontSize: 15,
+    fontSize: 16,
     color: '#222',
-    lineHeight: 23,
+    lineHeight: 24,
     marginVertical: 4,
   },
 });
@@ -18,10 +18,30 @@ type Props = {
   children?: any,
 };
 
+const levels = [, 1, 1.25, 1.5, 2, 4]
+
+function getLevelStyles(level) {
+  if (!level) {
+    return {};
+  }
+
+  const div = levels[level];
+
+  return {
+    fontSize: 24 / div,
+    lineHeight: 36 / div,
+    marginVertical: Math.floor(4 / div),
+  };
+}
+
 export default function Heading(props: Props) {
   return (
-    <Text {...props} style={[styles.heading, props.style]}>
+    <Text {...props} style={[styles.heading, props.style, getLevelStyles(props.level)]}>
       {props.children}
     </Text>
   );
 }
+
+Heading.defaultProps = {
+  level: 3,
+};
